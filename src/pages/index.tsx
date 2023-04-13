28812e2664384d1db9564016b71d0d72cbbf2de8
AustinSpinazze <aspinazze94@gmail.com>
2023-04-13 13:57:38 -0500

    Added React Hot Toast to show a toast on error when posting Added loading state for when post is being added so user cannot spam button Updated Zod to have custom error message

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,9 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -22,6 +23,20 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0], {
+          position: "bottom-center",
+          icon: "👎",
+        });
+      } else {
+        toast.error("Failed to post. Please try again.", {
+          position: "bottom-center",
+          icon: "👎",
+        });
+      }
     },
   });
 
@@ -38,13 +53,28 @@ const CreatePostWizard = () => {
       />
       <input
         placeholder="Type some emojis!"
-        className="grow bg-transparent outline-none"
+        className="grow bg-transparent text-xl outline-none"
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         disabled={isPosting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
