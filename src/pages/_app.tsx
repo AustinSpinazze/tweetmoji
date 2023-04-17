7b9bf3a1f0a1e355e48b37c887c6f11d3f43528e
AustinSpinazze <aspinazze94@gmail.com>
2023-04-16 21:33:42 -0500

    Added Profile page with SSG helper Added new PageLayout component Added dynamic meta data for pages Added new getByUsername endpoint Made filterUserForClient its own helper util

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,16 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Tweetmoji</title>
+        <meta name="description" content="Bird app but with just emojis 🤷‍♂️" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster />
       <Component {...pageProps} />
     </ClerkProvider>
